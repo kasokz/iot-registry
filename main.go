2144efa7a9f5ab0338a41b0649eb50b32b78e9fd
Long Bui <longbui68@yahoo.de>
2018-11-21 22:59:29 +0100

    add Functionality struct for functionality description of device

--- main.go
+++ main.go
@@ -5,19 +5,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type Device struct {
-	ID   string
-	Name string
-	Kind string
+// Functionality struct, defining device functionality
+type Functionality struct {
+	Description string `json:"desc"`
+	Path        string `json:"path"`
 }
 
+// Functionalities is a Functionality array
+type Functionalities []Functionality
+
+// Device structure
+type Device struct {
+	ID    string          `json:"id"`
+	Name  string          `json:"name"`
+	Funcs Functionalities `json:"funcs"`
+}
+
+// Devices is a Device array
 type Devices []Device
 
 func allDevices(w http.ResponseWriter, r *http.Request) {
 	devices := Devices{
-		Device{ID: "1", Name: "Ceiling Lightbulb", Kind: "LightBulb"},
+		Device{ID: "1", Name: "Ceiling Lightbulb", Funcs: Functionalities{
+			Functionality{Description: "Toggle on and off", Path: "/toggle"},
+		}},
 	}
 	fmt.Println("All Devices Endpoint hit!")
 	json.NewEncoder(w).Encode(devices)
@@ -28,9 +43,10 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequest() {
-	http.HandleFunc("/api", handleHome)
-	http.HandleFunc("/devices", allDevices)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter().StrictSlash(true).PathPrefix("/api").Subrouter()
+	router.HandleFunc("/", handleHome).Methods("GET")
+	router.HandleFunc("/devices", allDevices).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func main() {
